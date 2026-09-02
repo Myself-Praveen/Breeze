dc4460bce6cc2753a3cf544c8f585af0aad09fcb
Myself-Praveen <praveen104685@gmail.com>
2026-09-03 02:33:16 +0530

    Fix Ctrl+C not killing the PyQt app in terminal

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
+import signal
 from PyQt6.QtWidgets import QApplication
 from overlay_ui import ChatWindow
 from breeze_agent import BreezeAgent
 
 def main():
+    # Allow Ctrl+C to close the application in the terminal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
     app = QApplication(sys.argv)
     
     # Ensure the app doesn't quit if the transparent window is the only one open
